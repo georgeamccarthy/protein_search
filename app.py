6921644f8672da53f2136359143340b252405ded
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-06-30 16:54:33 +0100

    Add missing )

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import jina
 flow = jina.Flow(port_expose=45678, protocol='http').add(uses="jinahub://encoder").add(uses="jinahub://indexer")
 with flow:
-    flow.post(on="/index", inputs=jina.types.document.generators.from_csv('data/input.csv')
+    flow.post(on="/index", inputs=jina.types.document.generators.from_csv('data/input.csv'))
     flow.block()
