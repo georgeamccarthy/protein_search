a7420f87278894ac1886924a310825bf8bc25c89
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-07-02 18:23:02 +0100

    Create backend directory and move app.py into there. Remove calls to jinahub from app.py. Create backend_config file.

--- app.py
+++
@@ -1,5 +0,0 @@
-import jina
-flow = jina.Flow(port_expose=45678, protocol='http').add(uses="jinahub://encoder").add(uses="jinahub://indexer")
-with flow:
-    flow.post(on="/index", inputs=jina.types.document.generators.from_csv('data/input.csv'))
-    flow.block()
---
+++ backend/app.py
@@ -0,0 +1,1 @@
+from jina import Flow
