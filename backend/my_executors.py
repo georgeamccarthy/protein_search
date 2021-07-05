c2afeff590ec08ca705da8a9aa4e67698137b5f6
fissoreg <giancarlo.fissore@gmail.com>
2021-07-05 15:52:59 +0000

    Type annotation + reorganization and formatting.

--- backend/my_executors.py
+++ backend/my_executors.py
@@ -1,28 +1,32 @@
-#%%
+# %%
 import torch
 from transformers import BertModel, BertTokenizer
 import re
+from typing import Sequence, List
 
 from jina import Executor, requests
 from jina import Document, DocumentArray
 
+
 class ProtBertExecutor(Executor):
+    """ProtBERT executor: https://huggingface.co/Rostlab/prot_bert"""
+
     def __init__(self, **kwargs):
         super().__init__()
 
-        tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False )
+        tokenizer = BertTokenizer.from_pretrained(
+            "Rostlab/prot_bert", do_lower_case=False
+        )
         model = BertModel.from_pretrained("Rostlab/prot_bert")
 
         self.tokenizer = tokenizer
         self.model = model
 
-
     @requests
-    def encode(self, docs, **kwargs):
-        sequences = docs.get_attributes('text')
-        sequences = [re.sub(r"[UZOB]", "X", seq) for seq in sequences]
+    def encode(self, docs: DocumentArray, **kwargs) -> DocumentArray:
+        sequences = self.preprocessing(docs.get_attributes("text"))
+        encoded_inputs = self.tokenizer(sequences, return_tensors="pt")
 
-        encoded_inputs = self.tokenizer(sequences, return_tensors='pt')
         with torch.no_grad():
             outputs = self.model(**encoded_inputs)
             embeds = outputs.last_hidden_state[:, 0, :].detach().numpy()
@@ -30,12 +34,20 @@ class ProtBertExecutor(Executor):
                 doc.embedding = embed
 
         return docs
-#%%
+
+    def preprocessing(self, sequences: Sequence[str]) -> List[str]:
+        """The rare amino acids "U,Z,O,B" are mapped to "X".
+
+        :param sequences: a `Sequence` of proteins
+        :return: a `List` of proteins with "U,Z,O,B" replaced by "X".
+        """
+        return [re.sub(r"[UZOB]", "X", seq) for seq in sequences]
+
+
+# %%
 sequences = ["A E T C Z A O", "T E A B Z A O"]
-sequences = [re.sub(r"[UZOB]", "X", seq) for seq in sequences]
 
 docs = DocumentArray((Document(content=seq) for seq in sequences))
 
-#%%
-embedded_docs = ProtBertExecutor().encode(docs)
 # %%
+embedded_docs = ProtBertExecutor().encode(docs)
