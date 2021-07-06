70b903ff3f051a2dfa12a23da6730a65c08ad974
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-07-06 13:59:06 +0100

    Correct mapping string

--- backend/app.py
+++ backend/app.py
@@ -2,5 +2,5 @@ from jina.types.document.generators import from_csv
 from jina import DocumentArray
 
 proteins = DocumentArray(
-    from_csv(open("../data/Train_HHblits_1column.csv"), {"Sequences": "text"})
+    from_csv(open("../data/Train_HHblits_1column.csv"), {"Protein sequences": "text"})
 )
