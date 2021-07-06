b0791e600ac1760ff3691231e451591b4a94429a
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-07-06 13:33:24 +0100

    Fix da_from_csv docustring
    Following conventions https://www.python.org/dev/peps/pep-0257/

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,10 +1,8 @@
 
 from jina import DocumentArray, Document
 
-'''
-Creates a DocumentArray from a chosen column of a csv file.
-'''
 def da_from_csv(csv_path: str, text_loc: int=0) -> DocumentArray:
+    """Creates a DocumentArray from a chosen column of a csv file."""
     docs = []
     from csv import reader
     with open(csv_path, 'r') as datafile:
