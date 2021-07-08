94f346b8e222e2748b4bab5581b5ea7d21c3ff98
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-07-08 11:49:02 +0100

    Add test for new Indexer (only for same length proteins).

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -11,3 +11,19 @@ def test_ProtBERT_encoding():
     sequences = ["A E T C Z A O", "T E A B Z A O"]
     docs = DocumentArray((Document(content=seq) for seq in sequences))
     embedded_docs = ProtBertExecutor().encode(docs)
+
+# %%
+def test_Indexer_indexing_samelength():
+    proteins = DocumentArray(
+        from_csv(
+            fp=open("../data/samelength.csv"),
+            field_resolver={"Protein sequences": "text"},
+        )
+    )
+
+    flow = Flow(port_expose=12345).add(uses=ProtBertExecutor).add(uses=MyIndexer)
+    with flow:
+        flow.index(proteins)
+
+if __name__ == "__main__":
+    main()
