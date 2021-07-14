2646d87f9c8530bedb0941c96499194544364e67
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-07-14 13:41:10 +0100

    Set protein_path in backend_config.py. Reformat with black. Remove block (uneeded). Search with random protein from datafile (for development).

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,29 @@
 from jina.types.document.generators import from_csv
 from jina import DocumentArray, Flow
 from my_executors import ProtBertExecutor, MyIndexer
+from backend_config import protein_path
+from random import randint
+
 
 def main():
     proteins = DocumentArray(
         from_csv(
-            fp=open("data/samelength.csv"),
+            fp=open(protein_path),
             field_resolver={"Protein sequences": "text"},
         )
     )
+    protein = proteins[randint(0, len(proteins)-1)]
+    print(f'Searching with protein sequence:\n{protein.text}')
 
-    flow = Flow().add(uses=ProtBertExecutor).add(uses=MyIndexer)
+    flow = (
+        Flow()
+        .add(uses=ProtBertExecutor)
+        .add(uses=MyIndexer)
+    )
     with flow:
         flow.index(proteins)
-        flow.close()
+        flow.post('/search', protein)
+
 
 if __name__ == "__main__":
     main()
