e3089781101bc7568a9a113576f8e6f5315ee449
fissoreg <giancarlo.fissore@gmail.com>
2021-07-29 13:12:02 +0200

    1. Querying rcsb.org with `gql` to obtain metadata. 2. Integrated the metadata in the frontend. 3. Spinning proteins!

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,12 @@
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+from typing import List, Dict
 
 
-def get_data(query: str, endpoint: str) -> dict:
+def search(query: str, endpoint: str) -> dict:
     headers = {
         "Content-Type": "application/json",
     }
@@ -17,6 +20,88 @@ def get_data(query: str, endpoint: str) -> dict:
 
     return matches
 
+
+def parse_polymer(polymer: Dict):
+    try:
+
+        source_organisms = polymer["rcsb_entity_source_organism"]
+        parent_scientific_names = [
+            o["ncbi_parent_scientific_name"] for o in source_organisms
+        ]
+        scientific_names = [o["ncbi_scientific_name"] for o in source_organisms]
+        organisms = [
+            name + " (" + parent + ")"
+            for name, parent in zip(scientific_names, parent_scientific_names)
+        ]
+        # In principle the JSON response contains more then 1 organisms per source entity. I believe this is not possible in theory, or anyways it is probably not useful to take the thing into account. So we're gonna return only the first organism and we're happy.
+        return organisms[0]
+    except TypeError:
+        # If the organism information is not there.
+        return ""
+
+
+def parse_rcsb_response(response: Dict):
+    return [parse_rcsb_entry(e) for e in response["entries"]]
+
+def parse_rcsb_entry(entry: Dict):
+    citation = entry["rcsb_primary_citation"]
+    polymers = entry["polymer_entities"]
+    return {
+        "description": entry["struct"]["title"],
+        "release_date": entry["rcsb_accession_info"]["initial_release_date"][:10],
+        "doi": citation["pdbx_database_id_DOI"],
+        "publication": citation["title"],
+        "authors": entry["rcsb_primary_citation"]["rcsb_authors"],
+        "organisms": [parse_polymer(p) for p in polymers],
+    }
+
+
+def rcsb_metadata(ids: List[str], endpoint="https://data.rcsb.org/graphql"):
+    # Select your transport with a defined url endpoint
+    transport = AIOHTTPTransport(url=endpoint)
+
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport) #, fetch_schema_from_transport=True)
+
+    # Provide a GraphQL query
+    query = gql(
+        """
+    query rcsb_metadata ($ids: [String!]!)
+    { 
+      entries(entry_ids: $ids) {
+        struct {
+          title
+          pdbx_descriptor
+        }
+        rcsb_accession_info {
+          initial_release_date
+        }
+        rcsb_primary_citation {
+          pdbx_database_id_DOI
+          title
+          rcsb_authors
+        }
+        polymer_entities {
+          rcsb_entity_source_organism {
+            ncbi_parent_scientific_name
+            ncbi_scientific_name
+          }
+        }
+      }
+    }
+    """
+    )
+
+    # Execute the query on the transport
+    result = client.execute(query, variable_values={"ids": ids})
+
+    return result
+
+def pdb_metadata(ids: List[str]):
+    response = rcsb_metadata(ids)
+
+    return parse_rcsb_response(response)
+
 def protein_3d(pdb_id="1YCR", width=200, height=200):
     components.html(
         f"""
@@ -25,6 +110,7 @@ def protein_3d(pdb_id="1YCR", width=200, height=200):
         style='height: {height}px; width: {width}px; position: relative;'
         class='viewer_3Dmoljs'
         data-pdb='{pdb_id}'
+        data-spin='axis:y;speed:1'
         data-backgroundcolor='0xffffff'
         data-select1='chain:A'
         data-style1='cartoon:color=spectrum'
@@ -37,6 +123,7 @@ def protein_3d(pdb_id="1YCR", width=200, height=200):
         width=width,
     )
 
+
 endpoint = "http://localhost:12345/search"
 
 st.title("protein_search")
@@ -47,24 +134,32 @@ query = st.text_input(
 
 if st.button(label="Search") or query:
     if query:
-        matches = get_data(query, endpoint)
+        matches = search(query, endpoint)
         ids = [doc["id"] for doc in matches]
         scores = [doc["scores"]["cosine"]["value"] for doc in matches]
+        metadata = pdb_metadata(ids)
 
-        for id, score in zip(ids, scores):
+        for id, score, meta in zip(ids, scores, metadata):
             col1, col2 = st.beta_columns([1, 2])
             with col1:
                 protein_3d(pdb_id=id)
 
             with col2:
-                st.subheader("Properties")
+                st.header(id)
+                st.subheader(meta["description"])
                 st.markdown(
                     f"""
-                PDB ID: {id}\n
-                Similarity metric: {score:.2f}\n
-                [Explore properties](https://www.rcsb.org/structure/{id})
-              """,
+                *Release date*: {meta["release_date"]}\n
+                """)
+
+                organisms = ", ".join(filter(None, meta["organisms"]))
+                if organisms:
+                    st.markdown(f"*Organisms:* {organisms}\n")
+
+                st.markdown(f"""
+                  Similarity metric: {score:.2f}\n
+                  [Explore properties](https://www.rcsb.org/structure/{id})
+                """,
                 )
     else:
         st.markdown("Please enter a query")
-
