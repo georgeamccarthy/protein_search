8784a94af0b241fdbbdad91784850ec3e0adb76b
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-08-10 12:50:14 +0100

    feat 1: shuffle protein data on first index. feat 2: store prots in memmap.

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,17 +1,27 @@
 from backend_config import print_logs
+import pandas as pd
+import numpy as np
+
 
 def cull_duplicates(fp):
-    import pandas as pd
-
     df = pd.read_csv(fp)
-    df = df.drop_duplicates(subset=['structureId'])
-    df[['structureId', 'sequence']].to_csv(fp, index=False)
+    df = df.drop_duplicates(subset=["structureId"])
+    df[["structureId", "sequence"]].to_csv(fp, index=False)
+
 
 def download_csv(url, fp):
     import requests
+
     response = requests.get(url)
     with open(fp, "wb") as f:
         f.write(response.content)
+
+
+def shuffle_csv(fp):
+    df = pd.read_csv(fp)
+    np.random.shuffle(df.values)
+    df.to_csv(fp, index=False)
+
 
 def log(message):
     if print_logs:
