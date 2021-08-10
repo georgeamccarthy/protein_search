af5c9b66d3254e03d4e173df20f11790546647ae
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-08-10 13:20:04 +0100

    feat: log number of culled rows.

--- backend/helpers.py
+++ backend/helpers.py
@@ -5,7 +5,13 @@ import numpy as np
 
 def cull_duplicates(fp):
     df = pd.read_csv(fp)
+
+    # Store number of csv rows before and after culling dups (and log).
+    starting_length = df.shape[0]
     df = df.drop_duplicates(subset=["structureId"])
+    culled_length = df.shape[0]
+    log(f"Culled {starting_length - culled_length} duplicates.")
+
     df[["structureId", "sequence"]].to_csv(fp, index=False)
 
 
